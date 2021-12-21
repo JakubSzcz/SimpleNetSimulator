b9b660e4a73d8e80b5cda3fb848227c29bfcb62c
Marek <marekwypich@gmail.com>
2021-12-21 17:34:59 +0100

    zmodyfikowano Route, dodano metode add_route do RoutingTable

--- src/UnitTests/Devices/RouterTest.java
+++ src/UnitTests/Devices/RouterTest.java
@@ -1,9 +1,15 @@
 package UnitTests.Devices;
 
+import Devices.Route;
+import Devices.RouteCode;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RouterTest {
-
+    @Test
+    public void route_to_string(){
+        Route route = new Route(RouteCode.C, 0,1,1,1,0);
+        assertEquals("C ", route.to_string());
+    }
 }
