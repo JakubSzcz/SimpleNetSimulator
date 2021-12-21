11fdd98b357efebe85ab8a57af3a597fdc705c86
Jakub Szczudlik <jszczudlik@student.agh.edu.pl>
2021-12-21 19:24:05 +0100

    working parsing ip addres to int from String and from String to int + test

--- src/Protocols/IPv4.java
+++ src/Protocols/IPv4.java
@@ -11,7 +11,7 @@ public class IPv4 {
         return true;
     }
 
-    //parse ip address from String to long
+    //parse ip address from String dotted format to the long number
     public static long parse_to_long(String ip_address_string){
 
         //split ip_address_string into String ip_octets
@@ -29,10 +29,40 @@ public class IPv4 {
         return ip_address_long;
     }
 
-    //parse ip address from long to String
+    //parse ip address from long number to String dotted format
     public static String parse_to_string(long ip_address_long){
+        //change from decimal long to binary string
+        String ip_address_string = Long.toBinaryString(ip_address_long);
+        //add 0s to beginning of the binary address if it's length is different from 32
+        if(ip_address_string.length() < 32){
+            int to_add =  32-ip_address_string.length();
+            String zeros = "0";
+            for(int i = 0; i < to_add-1; i++){
+                zeros += "0";
+            }
+            ip_address_string = zeros + ip_address_string;
+        }
+        //inserting dots in specific position (index 8, 17, 26) in order to distinguish binary octets
+        StringBuilder ip_string_builder = new StringBuilder(ip_address_string);
+        for(int i = 1; i < 4; i++){
+           ip_string_builder.insert(i*8 + (i-1),".");
+        }
+        //converting binary strings to decimal ints
+        ip_address_string = ip_string_builder.toString();
+        int[] ip_octets_int = new int[4];
+        String[] ip_octets_string = ip_address_string.split("\\.");
+        for(int i = 0; i < 4; i++){
+            ip_octets_int[i] = Integer.parseInt(ip_octets_string[i], 2);
+            ip_octets_string[i] = String.valueOf(ip_octets_int[i]);
+        }
+        ip_address_string = ip_octets_string[0];
 
-        return "nic";
+        //converting decimal strings into one string ip dotted address
+        for(int i = 0; i < 3; i++){
+            ip_address_string += "." + ip_octets_string[i+1];
+        }
+
+        return ip_address_string;
     }
 
 }
--- src/UnitTests/Protocols/ProtocolsTest.java
+++ src/UnitTests/Protocols/ProtocolsTest.java
@@ -6,17 +6,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProtocolsTest {
-
+    //instance of IPv4 class
+    IPv4 ipv4 = new IPv4();
     @Test
     void to_string() {
     }
 
     @Test
-    //check if the string was correctly parsed to int
+    //check if the ip address in string was correctly parsed to the int type
     void parse_to_long() {
-        IPv4 ipv4 = new IPv4();
         assertEquals(3232235777l, ipv4.parse_to_long("192.168.1.1"));
         assertEquals(2888455179l, ipv4.parse_to_long("172.42.84.11"));
         assertEquals(0l, ipv4.parse_to_long("0.0.0.0"));
+    }
+
+    @Test
+    void is_valid() {
+    }
+
+    @Test
+    ////check if the ip address in int was correctly parsed to the string type
+    void parse_to_string() {
+        assertEquals("192.168.1.1", ipv4.parse_to_string(3232235777l));
+        assertEquals("172.42.84.11", ipv4.parse_to_string(2888455179l));
+        assertEquals("0.0.0.1", ipv4.parse_to_string(1l));
+        assertEquals("0.2.0.1", ipv4.parse_to_string(131073l));
     }
 }
