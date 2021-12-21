07664b688ac52ecb4c2061d1a4bfe245be415662
Jakub Szczudlik <jszczudlik@student.agh.edu.pl>
2021-12-21 17:55:57 +0100

    Ipv4 parse to int working + test

--- src/Protocols/IPv4.java
+++ src/Protocols/IPv4.java
@@ -1,5 +1,38 @@
 package Protocols;
 
 public class IPv4 {
+    /////////////////////////////////////////////////////////
+    //                     functions                       //
+    /////////////////////////////////////////////////////////
+
+    //return true if given address is correct
+    public static boolean is_valid(long ip_address){
+
+        return true;
+    }
+
+    //parse ip address from String to long
+    public static long parse_to_long(String ip_address_string){
+
+        //split ip_address_string into String ip_octets
+        String[] ip_octets_string = ip_address_string.split("\\.");
+
+        //parse octets(str) to long_octets and shift each of them in left, eventually sum up to ip_address_long
+        long[] ip_octets_long = new long[4];
+        long ip_address_long = 0;
+        for(int i = 0; i < 4; i++){
+            ip_octets_long[i] = Integer.parseInt(ip_octets_string[i]);
+            ip_octets_long[i] = ip_octets_long[i] << (24-8*i);
+            ip_address_long += ip_octets_long[i];
+        }
+
+        return ip_address_long;
+    }
+
+    //parse ip address from long to String
+    public static String parse_to_string(long ip_address_long){
+
+        return "nic";
+    }
 
 }
--- src/UnitTests/Protocols/ProtocolsTest.java
+++ src/UnitTests/Protocols/ProtocolsTest.java
@@ -1,5 +1,6 @@
 package UnitTests.Protocols;
 
+import Protocols.IPv4;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +9,14 @@ class ProtocolsTest {
 
     @Test
     void to_string() {
+    }
+
+    @Test
+    //check if the string was correctly parsed to int
+    void parse_to_long() {
+        IPv4 ipv4 = new IPv4();
+        assertEquals(3232235777l, ipv4.parse_to_long("192.168.1.1"));
+        assertEquals(2888455179l, ipv4.parse_to_long("172.42.84.11"));
+        assertEquals(0l, ipv4.parse_to_long("0.0.0.0"));
     }
 }
