6251c7a50d1a629db2a0b671d9270ea8a54cc478
Jakub Szczudlik <jszczudlik@student.agh.edu.pl>
2021-12-21 15:02:48 +0100

    added enum Layer3protocols, modified Frame and SimpleP2PFrame

--- src/Protocols/SimpleP2PFrame.java
+++ src/Protocols/SimpleP2PFrame.java
@@ -1,4 +1,26 @@
 package Protocols;
 
 public class SimpleP2PFrame implements Frame{
+
+    /////////////////////////////////////////////////////////
+    //                 variables and objects               //
+    /////////////////////////////////////////////////////////
+
+    //name of the layer 3 protocol
+    private final Layer3Protocols l3_protocol;
+
+    /////////////////////////////////////////////////////////
+    //                     functions                       //
+    /////////////////////////////////////////////////////////
+
+    //constructor
+    public SimpleP2PFrame() {
+        this.l3_protocol = Layer3Protocols.IPv4;
+    }
+
+    //print frame's content
+    @Override
+    public String to_string() {
+        return null;
+    }
 }
