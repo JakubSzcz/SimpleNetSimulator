634eafe9cf5f6f322d5decf46ed631e91a600e27
Marek <marekwypich@gmail.com>
2021-12-27 14:41:28 +0100

    basic GUI components

--- src/GUI/NetworkGUI.java
+++ src/GUI/NetworkGUI.java
@@ -1,4 +1,15 @@
 package GUI;
 
+import javax.swing.*;
+
 public class NetworkGUI {
+    private JPanel gui_panel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("NetworkGUI");
+        frame.setContentPane(new NetworkGUI().gui_panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
