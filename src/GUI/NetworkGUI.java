8d1749f6b5260006bcca5924289266f63fd1b8aa
Jakub Szczudlik <jszczudlik@student.agh.edu.pl>
2021-12-27 17:47:45 +0100

    beginning of NetworkGUI, buttons, Jpanels

--- src/GUI/NetworkGUI.java
+++ src/GUI/NetworkGUI.java
@@ -4,9 +4,24 @@ import javax.swing.*;
 
 public class NetworkGUI {
     private JPanel gui_panel;
+    private JButton add_link;
+    private JButton delete_link;
+    private JButton add_router;
+    private JButton delete_router;
+    private JButton save_topology;
+    private JButton load_topology;
+    private JPanel buttons;
+    private JPanel down_margin;
+    private JPanel left_margin;
+    private JPanel right_margin;
+    private JPanel topology_map;
+
+    public NetworkGUI() {
+        this.add_router.setBorder(BorderFactory.createEmptyBorder());
+    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("NetworkGUI");
+        JFrame frame = new JFrame("Network Topology");
         frame.setContentPane(new NetworkGUI().gui_panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
